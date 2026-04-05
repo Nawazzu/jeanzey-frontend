770ad73ebb6b54f7915962b4a8715211efbd330a
Nawazzu <136444577+Nawazzu@users.noreply.github.com>
2026-04-05 15:10:35 +0530

    feat:admin short order IDs

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.png'
+
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
@@ -15,7 +15,6 @@ import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
 
 export const assets = {
-    logo,
     cart_icon,
     dropdown_icon,
     exchange_icon,
